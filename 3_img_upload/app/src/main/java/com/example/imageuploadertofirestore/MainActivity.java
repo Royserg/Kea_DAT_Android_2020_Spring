348f24e63a1a3d83569b5c301aa4385a38d21032
Jakub <staniojakub@gmail.com>
2020-03-09 20:12:24 +0100

    week10-initial-project

--- 3_img_upload/app/src/main/java/com/example/imageuploadertofirestore/MainActivity.java
+++ 3_img_upload/app/src/main/java/com/example/imageuploadertofirestore/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.imageuploadertofirestore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.imageuploadertofirestore.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
+
+    /* Open gallery */
+    public void gelleryBtnPressed(View v) {
+        Toast.makeText(this, "Gallery Btn Pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    public void uploadToFirestore(View v) {
+
+    }
+
 }
